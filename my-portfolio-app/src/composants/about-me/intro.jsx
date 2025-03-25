7657916ee471c3ee675856eef84acdac14ded46d
Marie Orsoni <m.orsoni20220@gmail.com>
2025-03-25 20:46:43 +0100

    decided on light color theme green 478559, purple 161748, pink f95d9b, blue 39a0ca

--- my-portfolio-app/src/composants/about-me/intro.jsx
+++ my-portfolio-app/src/composants/about-me/intro.jsx
@@ -4,19 +4,14 @@ function AboutMe() {
   return (
     <>
       <div className="message-info">
-        <h2 className="message-header">A propos</h2>
+        <h2 className="message-header">
+          {" "}
+          Ensemble, donnons vie à vos idées digitales.
+        </h2>
 
         <p className="message">
-          Passionnée par l’intégration de sites web et le développement de
-          fonctionnalités personnalisées, je m’adapte aux demandes spécifiques
-          de mes clients. Forte d’une expérience en vente et marketing,
-          j’optimise le référencement SEO de vos projets afin de les rendre plus
-          visibles sur les moteurs de recherche et de mieux cibler votre
-          clientèle. Chaque projet est unique et mérite une attention
-          particulière. Je prends le temps d’écouter et d’échanger avec mes
-          clients pour comprendre leur activité, leur entreprise et leurs
-          ambitions. Ensemble, nous formons une véritable équipe pour
-          concrétiser vos idées et atteindre vos objectifs.
+          Every great digital experience starts with an idea. Let’s bring yours
+          to life.
         </p>
       </div>
     </>
