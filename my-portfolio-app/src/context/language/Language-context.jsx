f08cad3c40c920b894189ff2fc7b9aa3349f85c8
Marie Orsoni <m.orsoni20220@gmail.com>
2025-04-08 19:21:38 +0200

    Updated the language context to with a content fetch from the json file to ease the use of the context on the app

--- my-portfolio-app/src/context/language/Language-context.jsx
+++ my-portfolio-app/src/context/language/Language-context.jsx
@@ -1,17 +1,42 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const LanguageContext = createContext();
 
-const LanguageProvider = ({ children }) => {
+export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState("fr");
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    const storedLang = localStorage.getItem("lang");
+    if (storedLang) setLanguage(storedLang);
+  }, []);
 
   const toggleLanguage = () => {
-    setLanguage((prev) => (prev === "fr" ? "en" : "fr"));
+    const newLang = language === "fr" ? "en" : "fr";
+    setLanguage(newLang);
+    localStorage.setItem("lang", newLang);
   };
 
+  useEffect(() => {
+    const fetchContent = async () => {
+      try {
+        const response = await fetch("./dev-portfolio.json");
+        if (!response.ok) {
+          throw new Error("Failed to fetch translation file");
+        }
+        const data = await response.json();
+        setContent(data);
+      } catch (error) {
+        console.error("Language fetch error:", error);
+        setContent({});
+      }
+    };
+    fetchContent();
+  }, [language]);
+
   return (
-    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+    <LanguageContext.Provider value={{ language, toggleLanguage, content }}>
       {children}
     </LanguageContext.Provider>
   );
@@ -20,4 +45,4 @@ LanguageProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-export { LanguageProvider, LanguageContext };
+export { LanguageContext };
